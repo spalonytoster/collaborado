a803289ae6918c579488fa25e86005d104a7ec19
ktyminski <karoltyminskikarol@gmail.com>
2016-11-12 00:49:19 +0100

    zrobiony routing na login

--- client/ng/components/login/login.component.js
+++ client/ng/components/login/login.component.js
@@ -5,6 +5,17 @@ import template from './login.html';
 
 const name = 'login';
 
+
 Login.component(name, {
   template
-});
+})
+.config(config);
+
+function config($stateProvider) {
+'ngInject';
+$stateProvider
+  .state('login', {
+    url: '/login',
+    template: '<login></login>'
+  });
+}
