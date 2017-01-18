3ded6a326616f62f1cb0b5d740135421f751f510
Maciej Posłuszny <xmachiei@gmail.com>
2017-01-18 14:03:18 +0100

    header wyswietla teraz email i displayname uzytkownika

--- client/ng/components/sidebar/menu-header/menu-header.component.js
+++ client/ng/components/sidebar/menu-header/menu-header.component.js
@@ -1,15 +1,35 @@
 // jshint esversion: 6
 import module from './menu-header.module';
 import template from './menu-header.html';
+import { Meteor } from 'meteor/meteor';
 
 class MenuHeader {
-  constructor($mdDialog) {
+  constructor($mdDialog, $scope, $reactive) {
     'ngInject';
     this._mdDialog = $mdDialog;
+
+    $reactive(this).attach($scope);
+
+    this.helpers({
+      isLoggedIn() {
+        return _.isString(Meteor.userId());
+      },
+      currentUser() {
+        return Meteor.user();
+      },
+      email() {
+        if (_.isUndefined(Meteor.user())) { return undefined; }
+        return _.first(Meteor.user().emails).address;
+      },
+      displayName() {
+        if (_.isUndefined(Meteor.user())) { return undefined; }
+        let profile = Meteor.user().profile;
+        return `${profile.name} ${profile.surname}`;
+      }
+    });
   }
 
   openSettings(event) {
-
     let config = {
       parent: angular.element(document.body),
       targetEvent: event,
@@ -23,7 +43,6 @@ class MenuHeader {
       escapeToClose: true,
       focusOnOpen: true
     };
-
     this._mdDialog.show(config);
   }
 }
